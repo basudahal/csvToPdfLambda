9c87832581ece4ead6995dc83bc0fdae4a094f97
System Administrator <root@basudevs-air.lan>
2020-11-27 14:57:26 -0700

    jenkinfile added

--- jenkinfile.groovy
+++ jenkinfile.groovy
@@ -1,53 +1,49 @@
 pipeline {
-	agent any	 
-	
-	stages {
-		stage('Unit test') {
-			steps {				
- 			    sh 'serverless --help' // to ensure it is installed
-			}
-		}			
-		
-		stage('Integration test') {
-			steps {
-				sh 'serverless deploy --stage dev'
-				sh 'serverless invoke --stage dev --function hello'					
-			}
-		}
-				
-		
-	 	stage('Production') {
-			when {
-			   env.BRANCH_NAME == 'master'	
-			}
-			steps {	
-			    parallel (
-				    'us-east-1' : {
-					  sh 'serverless deploy --stage production --region us-east-1'  
-					  sh 'serverless invoke --stage production --region us-east-1 --function hello'
-				    },
-				    'ap-southeast-2' : {
-					  sh 'serverless deploy --stage production --region ap-southeast-2'  
-					  sh 'serverless invoke --stage production --region ap-southeast-2 --function hello'  
-				    }
-				    
-		            )
-			}	
-		}
-		
-		stage('Teardown') {
-			steps {				
-				echo 'No need for DEV environment now, tear it down'
-				sh 'serverless remove --stage dev'	
-			}
-		}
-	 
-	 }
-	
-	
-	 environment {
-	 		AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
-			AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
-	 }
+  agent any
 
+  environment {
+    subject = ''
+    projectName = 'lambda-csvToPdf'
+    deploymentRole = 'deployment-role'
+    deploymentCredentialId = 'jenkin_svc'
+    deploymentAccount = '878955458484'
+  }
+  parameters {
+    string(description: 'dev tag', name: 'dev_tag')
+    choice(name: 'region', choices: ['us-east-1', 'us-east-2'], description: 'Region to deploy')
+    choice(name: 'environment', choices: [dev], description: 'Environment to deploy')
+  }
+
+  stages{
+    stage('Prepare'){
+      steps {
+        buildName "${params.dev_tag}-${params.environment}"
+        sh "git checkout ${params.dev_tag}"
+        script {
+          deployCommand = "serverless deploy"
+          deployAccount = deploymentAccount
+        }
+      }
+    }
+    stage('npm install') {
+      steps {
+        sh "npm install"
+      }
+    }
+    stage('Deploy') {
+      steps {
+        script {
+          env.AWS_ACCESS_KEY_ID = ''
+          env.AWS_SECRET_ACCESS_KEY = ''
+          withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: "${deploymentCredentialId}", accessKeyVariable:'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+            def assume_result = sh returnStdout:true, script: "aws sts assume-role --role-arn arn:aws:iam::${deployAccount}:role/${deploymentRole} --role-session-name ${projectName}-deploy"
+            def assume_object = Json.parse(assume_result)
+            env.AWS_ACCESS_KEY_ID = assume_object.Credentials.AccessKeyId
+            env.AWS_SECRET_ACCESS_KEY = assume_object.Credentials.SecretAccessKey
+          }
+        }
+        sh deployCommand
+      }
+    }
+  }
 }
